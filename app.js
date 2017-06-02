50272efcf3e457b1aa01b4ed6d2ffb55fa737b45
soraya2 <soraya_ibrahim@live.nl>
2017-06-02 18:31:11 +0200

    fixed XO

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express');
 var path = require('path');
+var http = require('http');
+var express = require('express');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -8,39 +9,39 @@ var lessMiddleware = require('less-middleware');
 var env = require('dotenv').config();
 var sessions = require('express-session');
 var mongoose = require('mongoose');
-var http = require('http');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server); // Use socket io in seperate files
-// view engine setup
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+// View engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sessions({
-    secret: process.env.EXPRESS_SESSION_SECRET,
+  secret: process.env.EXPRESS_SESSION_SECRET,
     // Name: cookie_name,
-    proxy: true,
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: true }
+  proxy: true,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: true}
 }));
 
-// app.use(passport.initialize());
+// App.use(passport.initialize());
 // app.use(passport.session());
 mongoose.connect(process.env.USERDB);
 
-// console.log(mongoose.connection.readyState); //test database connection
+// Console.log(mongoose.connection.readyState); //test database connection
 
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -48,25 +49,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Error handler
+app.use(function (err, req, res) {
+    // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-
-server.listen(app.get('port'), function() {
-    console.log('app started on localhost:3006');
+server.listen(app.get('port'), function () {
+  console.log('app started on localhost:3006');
 });
--- models/seriesschema.js
+++ models/seriesschema.js
@@ -4,53 +4,52 @@ var bcrypt = require('bcrypt-nodejs');
 
 var seriesSchema = mongoose.Schema({
 
-    user: {
-        facebook: {
-            id: String,
-            token: String,
-            secret: String,
-            displayName: String,
-            username: String,
-            friends: Array
-        },
-
-        google: {
-            id: String,
-            token: String,
-            secret: String,
-            displayName: String,
-            username: String,
-            calender: Array
-        },
-
-        profile: {
-            messages: Array,
-            preferences: Array
-        }
+  user: {
+    facebook: {
+      id: String,
+      token: String,
+      secret: String,
+      displayName: String,
+      username: String,
+      friends: Array
     },
 
-    admin: {
-        name: String,
-        reviews: Array
-
+    google: {
+      id: String,
+      token: String,
+      secret: String,
+      displayName: String,
+      username: String,
+      calender: Array
     },
 
-    series: {
-        data: Array
+    profile: {
+      messages: Array,
+      preferences: Array
     }
+  },
 
+  admin: {
+    name: String,
+    reviews: Array
+
+  },
+
+  series: {
+    data: Array
+  }
 
 });
 
 // Methods ======================
 // generating a hash
-seriesSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+seriesSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // Checking if password is valid
-seriesSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+seriesSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password);
 };
 
 // Create the model for users and expose it to our app

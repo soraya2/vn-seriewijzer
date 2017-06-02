0f3afb10b473978d38b886f968832eaf8750af09
soraya2 <soraya_ibrahim@live.nl>
2017-06-02 18:15:50 +0200

    added check and updated the database

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,10 @@ router.get('/', function(req, res) {
     getData(receiveData, '');
 
     function receiveData(data) {
+        if (data) {
 
-        res.render('index', { data: data, title: 'Home' });
+            res.render('index', { data: data, title: 'Home' });
+        }
 
     }
 
@@ -17,15 +19,15 @@ router.get('/', function(req, res) {
 
 function getData(recieve, value) {
 
-    request.get('https://api.themoviedb.org/3/discover/tv?api_key=' + env.parsed.MOVIEDBKEY + '&format=json' + value, function(error, response, body) {
-        // console.log('error:', error); // Print the error if one occurred
+    request.get('https://api.themoviedb.org/3/discover/tv?api_key=' + env.parsed.MOVIEDBKEY + '&format=json&page=2' + value, function(error, response, body) {
+        if (error) {
+            console.log('error:', error); // Print the error if one occurred
+        }
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
+
         var data = JSON.parse(body);
-        // recieve(data);
 
         saveData(data, recieve);
-        // console.log(env.parsed);
 
     });
 
@@ -33,28 +35,18 @@ function getData(recieve, value) {
 
 function saveData(data, recieve) {
     var query = 'data';
-
+    // if the data field exist update it and only add new values to the array so there are no duplicates
     seriesSchema.findOneAndUpdate(query, {
         "$addToSet": {
-            "series.data": data.results
-
-
+            "series.data": { $each: data.results }
         }
     }, { upsert: true }, function(err, document) {
-        // console.log(document.series.data[0]);
 
         if (err) return console.log(err);
 
-        recieve(document.series.data[0]);
-
-
-
-        // io.sockets.in('timer').emit('time', { info: document.twitter.tweets });
-
+        recieve(document.series.data);
 
     });
-
-
 }
 
 module.exports = router;

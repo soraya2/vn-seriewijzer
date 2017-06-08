59735bc333986d13d93314d256caa7f85ee3fd32
soraya2 <soraya_ibrahim@live.nl>
2017-06-08 21:05:45 +0200

    change route form

--- routes/persona.js
+++ routes/persona.js
@@ -1,23 +1,16 @@
 var express = require('express');
-var formidable = require('formidable');
-var util = require('util');
-
 var router = express.Router();
+var env = require('dotenv').config();
 
 router.get('/', function(req, res) {
     res.render('persona');
 });
 
-var express = require('express');
-
-var router = express.Router();
-
-var env = require('dotenv').config();
 
 router.post('/', function(req, res) {
-    // de waar
+    // store form data in route
     res.locals.step1 = req.body;
-    // res.render('index', { title: 'Home' });
+    res.send('hello');
 
 });
 

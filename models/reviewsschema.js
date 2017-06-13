708eefea0c9b5fd7676f19272f7779808b8aee1f
soraya2 <soraya_ibrahim@live.nl>
2017-06-13 12:43:55 +0200

    added basic comment section

--- models/reviewsschema.js
+++ models/reviewsschema.js
@@ -2,38 +2,39 @@ var mongoose = require('mongoose');
 
 var reviewsSchema = mongoose.Schema({
 
-  user: {
-    name:           String,
-    email:          String,
-    postDate:       String
-  },
+    user: {
+        name: String,
+        email: String,
+        postDate: String
+    },
 
-  review: {
-    seriesName:     String,
-    region:         String,
-    startYear:      String,
-    endYear:        String,
-    genre:          Array,
-    platform:       String,
-    period:         Array,
-    persona:        Array,
-    hobby:          Array,
-    mood:           Array,
-    ageRestriction: String,
-    seasons:        String,
-    episodes:       String,
-    duration:       String,
-    producers:      String,
-    awards:         String,
-    nominations:    String,
-    imdbRating:     String,
-    trailerURL:     String,
-    imgURL:         String,
-    plot:           String,
-    reviewBody:     String,
-    rating:         String
-  }
+    review: {
+        seriesName: String,
+        region: String,
+        startYear: String,
+        endYear: String,
+        genre: Array,
+        platform: String,
+        period: Array,
+        persona: Array,
+        hobby: Array,
+        mood: Array,
+        ageRestriction: String,
+        seasons: String,
+        episodes: String,
+        duration: String,
+        producers: String,
+        awards: String,
+        nominations: String,
+        imdbRating: String,
+        trailerURL: String,
+        imgURL: String,
+        plot: String,
+        reviewBody: String,
+        rating: String
+    },
 
+    comments: Array
 });
 
 module.exports = mongoose.model('Review', reviewsSchema);

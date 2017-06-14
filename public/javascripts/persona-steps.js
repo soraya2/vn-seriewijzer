9d724dddf171c13c33a632f9c74db381d2369c9c
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-06-14 15:06:09 +0200

    Created function that will disable checkboxes that arent visible

--- public/javascripts/persona-steps.js
+++ public/javascripts/persona-steps.js
@@ -1,3 +1,4 @@
+// BEGIN SORAYA
 (function() {
     if (document.getElementsByTagName('persona-check')) {
         var personaSteps = document.getElementsByClassName('persona-check');
@@ -13,18 +14,22 @@
         for (i = 1; i < personaSteps.length; i++) {
             personaSteps[i].classList.add('fade-in');
             personaSteps[i].classList.add('fade-out');
+
+            toggleCheckboxes(i, true);
         }
 
         personaSteps[0].classList.add('fade-in');
 
         function nextStep(e) {
             e.preventDefault();
-            console.log();
+
             switch (count) {
 
                 case 1:
                     personaSteps[0].classList.add('fade-out');
+                    toggleCheckboxes(0, true);
                     personaSteps[count].classList.remove('fade-out');
+                    toggleCheckboxes(count, false);
                     break;
 
                 case 2:
@@ -33,14 +38,27 @@
 
                     for (index = 0; index < personaSteps.length; index++) {
                         personaSteps[index].classList.add('fade-out');
+                        toggleCheckboxes(index, true);
                     }
 
+                    toggleCheckboxes(count, false);
                     personaSteps[count].classList.remove('fade-out');
             }
 
             return personaSteps[count++];
         }
 
+        // BEGIN CHANEL
+        function toggleCheckboxes(el, value){
+            var checkboxes = document.querySelectorAll('.persona-check:nth-of-type(' + (el + 1) + ') input[type="checkbox"]');
+
+            for(var checkbox of checkboxes){
+                checkbox.disabled = value;
+            }
+        }
+        // END CHANEL
+
         personaButton.addEventListener('click', nextStep);
     }
 }());
+// END SORAYA

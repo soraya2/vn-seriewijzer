8a5e7e98a02c33f4e9f380676cbb55a493d7e3f0
soraya2 <soraya_ibrahim@live.nl>
2017-06-20 18:41:40 +0200

    dynamic filter

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,17 @@
-var express = require('express');
-
-var router = express.Router();
-
-var env = require('dotenv').config();
-
-var reviewsSchema = require('../models/reviewsschema');
+var express = require('express'),
+    router = express.Router(),
+    env = require('dotenv').config(),
+    reviewsSchema = require('../models/reviewsschema');
 // var serieData = [{ 'name': 'a', 'color': 'black', "tagshobby": ["gamen", "shoppen"] }, { 'name': 'b', 'color': 'blue', "tagshobby": ["reizen"] }, { 'name': 'c', 'color': 'black', "tagshobby": ["muziek", "sporten"] }];
 
-var filters = {};
-var i;
-var hobbyAray = [];
-var peronalityAray = [];
-var moodsArray = [];
+var filters = {},
+    hobbyAray = [],
+    peronalityAray = [],
+    moodsArray = [],
+    i,
+    personaForm,
+    key;
+
 
 router.get('/', function(req, res) {
     reviewsSchema.find("review", function(err, docs) {
@@ -23,21 +23,35 @@ router.get('/', function(req, res) {
 
         // console.log(req.body.persona, "personaform");
 
-        if (req.session.personaform) {
+        // if (req.session.personaform) {
 
-            filterPersonality(req.session.personaform);
-            // console.log(filters);
+        //     filterPersonality(req.session.personaform);
+        //     // console.log(filters);
+        // }
+
+        // console.log();
+        function setFilter(filtername, filtervalue) {
+            filters[filtername] = filtervalue;
+
         }
 
 
+        for (key in req.session.personaform) {
+            if (req.session.personaform.hasOwnProperty(key)) {
+                setFilter(key, req.session.personaform[key]);
+            }
+        }
+
+
+        // var filterdData = [];
 
         var filterdData = docs.filter(function(serie) {
             return Object.keys(filters).every(function(key) {
 
-
-                return filters.persona[key].some(function(filterOptions) {
+                return filters[key].some(function(filterOptions) {
 
                     return serie.review[key].some(function(seriesTags) {
+                        console.log(filterOptions, seriesTags);
 
                         return filterOptions === seriesTags;
                     });
@@ -46,98 +60,35 @@ router.get('/', function(req, res) {
             });
         });
 
+
         console.log(filterdData, "filterdata");
         res.render('profile', { title: 'Home', data: filterdData, name: 'req.session.user' });
 
     });
 });
 
-// begin soraya
-router.post('/', function(req, res) {
 
-    // console.log(Array.isArray(req.body.tagshobby) === false);
-
-    // if (Array.isArray(req.body.tagshobby) === false) {
-    //     hobbyAray.push(req.body.tagshobby);
-
-    //     return filterHobby(hobbyAray);
-
-    // }
-    // if (req.body.tagshobby && Array.isArray(req.body.tagshobby)) {
-    //     hobbyAray.push(req.body.tagshobby);
-
-    //     filterHobby(req.body.tagshobby);
-    //     // for (i = 0; i < req.body.tagshobby.length; i++) {
-    //     //     hobbyAray.push(req.body.tagshobby[i]);
-    //     // }
-
-
-    // }
-    //--------------------------------------------------------
-    //fake data filter werkt niet echt!!
-    // var filterdData = [];
-
-    // Get random friends from user account
-    // var arr = [];
-    // var randomnumber = Math.floor(Math.random() * series2.length);
-    // while (arr.length < randomnumber) {
-    //     var randomnumber2 = Math.floor(Math.random() * series2.length);
-    //     if (arr.indexOf(randomnumber2) > -1) {
-    //         continue;
-    //     }
-    //     arr[arr.length] = randomnumber2;
-    // }
-    // for (var i = 0; i < arr.length; i++) {
-    //     filterdData.push(series2[arr[i]]);
-    // }
-
-
-    //------------------------------------------------------
-
-    // if (req.body.persona.tagsikwil) {
-
-    //     filterMoods([req.body.tagsikwil]);
-    // }
-
-
-
-
-
-
-
-
-    //     function receiveData(data) {
-    //         if (req.user !== undefined) {
-
-    //             res.render('profile', { title: 'Home', data: data, name: req.user.user.facebook.displayName });
-    //         } else {
-    //             res.render('login2');
-    //         }
-    //     }
-    // });
-
-});
 
 
 // add object array per filter
-function filterPersonality(filterOption) {
-    filters.persona = filterOption;
+// function filterPersonality(filterOption) {
+//     filters.persona = filterOption;
 
 
-    console.log(filters.persona, "[filterpersoonlijkheid]");
-    return filters.persona;
-}
+//     console.log(filters.persona, "[filterpersoonlijkheid]");
+//     return filters.persona;
+// }
 
-function filterHobby(filterOption) {
-    filters.hobby = filterOption;
-    console.log(filters.hobby, "[tagshobby]");
-    return filters.hobby;
-}
+// function filterHobby(filterOption) {
+//     filters.hobby = filterOption;
+//     console.log(filters.hobby, "[tagshobby]");
+//     return filters.hobby;
+// }
 
-function filterMoods(filterOption) {
-    filters.mood = filterOption;
-    console.log(filters.mood, "[tagsikwil]");
-    return filters.mood;
-}
+// function filterMoods(filterOption) {
+//     filters.mood = filterOption;
+//     console.log(filters.mood, "[tagsikwil]");
+//     return filters.mood;
+// }
 
 module.exports = router;

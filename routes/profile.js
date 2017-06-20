a17afd0edfcba0def3fa82ed859944e7f4bdb562
soraya2 <soraya_ibrahim@live.nl>
2017-06-20 20:04:42 +0200

    changed eps values and removed clutter

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,8 @@
 var express = require('express'),
     router = express.Router(),
     env = require('dotenv').config(),
-    reviewsSchema = require('../models/reviewsschema');
-// var serieData = [{ 'name': 'a', 'color': 'black', "tagshobby": ["gamen", "shoppen"] }, { 'name': 'b', 'color': 'blue', "tagshobby": ["reizen"] }, { 'name': 'c', 'color': 'black', "tagshobby": ["muziek", "sporten"] }];
-
-var filters = {},
+    reviewsSchema = require('../models/reviewsschema'),
+    filters = {},
     hobbyAray = [],
     peronalityAray = [],
     moodsArray = [],
@@ -12,29 +10,11 @@ var filters = {},
     personaForm,
     key;
 
-
 router.get('/', function(req, res) {
     reviewsSchema.find("review", function(err, docs) {
         if (err) {
             return err;
         }
-
-        // console.log(req.session.persona.tagspersoonlijkheid);
-
-        // console.log(req.body.persona, "personaform");
-
-        // if (req.session.personaform) {
-
-        //     filterPersonality(req.session.personaform);
-        //     // console.log(filters);
-        // }
-
-        // console.log();
-        function setFilter(filtername, filtervalue) {
-            filters[filtername] = filtervalue;
-
-        }
-
 
         for (key in req.session.personaform) {
             if (req.session.personaform.hasOwnProperty(key)) {
@@ -42,16 +22,13 @@ router.get('/', function(req, res) {
             }
         }
 
-
-        // var filterdData = [];
-
         var filterdData = docs.filter(function(serie) {
-            return Object.keys(filters).every(function(key) {
 
-                return filters[key].some(function(filterOptions) {
+            return Object.keys(filters).every(function(key) { // For each key in filter return filter key value
+
+                return filters[key].some(function(filterOptions) { // Compare filter options with serie tags
 
                     return serie.review[key].some(function(seriesTags) {
-                        console.log(filterOptions, seriesTags);
 
                         return filterOptions === seriesTags;
                     });
@@ -60,36 +37,14 @@ router.get('/', function(req, res) {
             });
         });
 
-
-        console.log(filterdData, "filterdata");
         res.render('profile', { title: 'Home', data: filterdData, name: 'req.session.user' });
 
     });
 });
 
+function setFilter(filtername, filtervalue) {
+    filters[filtername] = filtervalue;
 
-
-
-// add object array per filter
-// function filterPersonality(filterOption) {
-//     filters.persona = filterOption;
-
-
-//     console.log(filters.persona, "[filterpersoonlijkheid]");
-//     return filters.persona;
-// }
-
-// function filterHobby(filterOption) {
-//     filters.hobby = filterOption;
-//     console.log(filters.hobby, "[tagshobby]");
-//     return filters.hobby;
-// }
-
-// function filterMoods(filterOption) {
-//     filters.mood = filterOption;
-//     console.log(filters.mood, "[tagsikwil]");
-//     return filters.mood;
-// }
-
+}
 
 module.exports = router;

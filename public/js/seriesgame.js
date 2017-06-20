349267abfd90dabb61c3311e34d0de3993264c99
Shyanta Vleugel <shyantavleugel@gmail.com>
2017-06-20 12:03:35 +0200

    Get tags from number One series

--- public/js/seriesgame.js
+++ public/js/seriesgame.js
@@ -1,29 +1,29 @@
 (function() {
     'use-strict';
-    var serieChoices = document.getElementsByTagName('label');
-    console.log(serieChoices);
+    var serieChoicesInput = document.querySelectorAll('input[type="checkbox"]');
+    var serieChoicesLabel = document.querySelectorAll('label');
     var numbersArr = ['one', 'two', 'three', 'four'];
     var nextBtn = document.getElementById('next-btn');
     nextBtn.disabled = true;
 
-    for (var i = 0; i < serieChoices.length; i++) {
-        serieChoices[i].addEventListener('click', function(){
+    for (var i = 0; i < serieChoicesInput.length; i++) {
+        serieChoicesInput[i].addEventListener('click', function(){
             console.log(this);
             // Check if there already is an active class, if so, remove it
             if (document.querySelector('.active')){
                 document.querySelector('.active').removeAttribute('class', 'active');
             }
             // Check if it already has an ID, if so, remove it, else, set the attribute
-            if ((this.id).length > 0){
-                this.removeAttribute('id');
+            if ((this.parentNode.id).length > 0){
                 this.removeAttribute('name');
-                this.classList.remove('active');
+                this.parentNode.removeAttribute('id');
+                this.parentNode.removeAttribute('class','active');
             } else {
                 for (var i = numbersArr.length; i >= 0; i--) {
                     if(document.getElementById(numbersArr[i]) === null){
-                        this.setAttribute('id', numbersArr[i]);
                         this.setAttribute('name', numbersArr[i]);
-                        this.setAttribute('class', 'active');
+                        this.parentNode.setAttribute('id', numbersArr[i]);
+                        this.parentNode.setAttribute('class', 'active');
                     }
                 }
             }
--- routes/series-game.js
+++ routes/series-game.js
@@ -1,9 +1,15 @@
+// Needed requires for this feature
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Reviews = require('../models/reviewsschema');
+
+// Global variables config to make the game possible
 var reviewArr;
+var hobby = [];
+var mood = [];
+var persona = [];
 
 Reviews.find("review", function(err, docs) {
     if (err) {
@@ -16,9 +22,6 @@ Reviews.find("review", function(err, docs) {
 router.get('/', function (req, res) {
   res.render('series-game/intro');
 });
-router.post('/1', function(req, res){
-    console.log(req.body);
-})
 router.get('/1', function (req, res) {
     var step1 = [
         {   data: reviewArr.find(o => o.review.seriesName.toLowerCase() === 'fawlty towers')},
@@ -28,9 +31,16 @@ router.get('/1', function (req, res) {
     ]
     res.locals.stepNum = 1;
     res.locals.stepData = step1;
-    res.locals.stepNext = 2;
     res.render('series-game/steps');
-})
+});
+router.post('/1', function(req, res){
+    hobby = reviewArr.find(o => o.review.seriesName === req.body.one).review.hobby;
+    mood = reviewArr.find(o => o.review.seriesName === req.body.one).review.mood;
+    persona = reviewArr.find(o => o.review.seriesName === req.body.one).review.persona;
+    console.log(req.body.one);
+    console.log(hobby);
+    res.redirect('2');
+});
 router.get('/2', function (req, res) {
     var step2 = [
         {   data: reviewArr.find(o => o.review.seriesName.toLowerCase() === 'doctor who')},
@@ -40,9 +50,17 @@ router.get('/2', function (req, res) {
     ]
     res.locals.stepNum = 2;
     res.locals.stepData = step2;
-    res.locals.stepNext = 3;
     res.render('series-game/steps');
-})
+});
+router.post('/2', function(req, res){
+    hobby = hobby.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.hobby);
+    mood = mood.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.mood);
+    persona = persona.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.persona);
+
+    console.log(req.body.one);
+    console.log(hobby);
+    res.redirect('3');
+});
 router.get('/3', function (req, res) {
     var step3 = [
         {   data: reviewArr.find(o => o.review.seriesName.toLowerCase() === 'sopranos')},
@@ -52,9 +70,17 @@ router.get('/3', function (req, res) {
     ]
     res.locals.stepNum = 3;
     res.locals.stepData = step3;
-    res.locals.stepNext = 4;
     res.render('series-game/steps');
-})
+});
+router.post('/3', function(req, res){
+    hobby = hobby.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.hobby);
+    mood = mood.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.mood);
+    persona = persona.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.persona);
+
+    console.log(req.body.one);
+    console.log(hobby);
+    res.redirect('4');
+});
 router.get('/4', function (req, res) {
     var step4 = [
         {   data: reviewArr.find(o => o.review.seriesName.toLowerCase() === 'sherlock')},
@@ -64,16 +90,51 @@ router.get('/4', function (req, res) {
     ]
     res.locals.stepNum = 4;
     res.locals.stepData = step4;
-    res.locals.stepNext = 'overview';
     res.render('series-game/steps');
-})
+});
+router.post('/4', function(req, res){
+    hobby = hobby.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.hobby);
+    mood = mood.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.mood);
+    persona = persona.concat(reviewArr.find(o => o.review.seriesName === req.body.one).review.persona);
 
+    console.log(req.body.one);
+    console.log(hobby);
+    res.redirect('overview');
+});
 router.get('/details/:id', function (req, res) {
     Reviews.findOne({ 'review.seriesName': req.params.id }, function(err, series) {
         console.log('Found show: ' + series);
         res.render('series-game/detail-view', { data: series });
     });
-
-
 });
+router.get('/overview', function (req, res) {
+    var results = [];
+
+    hobbyUnique = hobby.filter(function( el, pos, self){
+        return self.indexOf(el) == pos;
+    });
+    moodUnique = mood.filter(function( el, pos, self){
+        return self.indexOf(el) == pos;
+    });
+    personaUnique = persona.filter(function( el, pos, self){
+        return self.indexOf(el) == pos;
+    });
+
+    console.log(reviewArr.length);
+    for (var i = 0; i < reviewArr.length; i++) {
+        var hobbyArr = reviewArr[i].review.hobby;
+        console.log(reviewArr[i].review.hobby);
+            if((hobbyArr.includes(hobby[0]) && hobbyArr.includes(hobby[1])) || (hobbyArr.includes(hobby[2]) && hobbyArr.includes(hobby[3]))){
+                results.push(reviewArr[i]);
+            }
+    }
+    console.log(results);
+
+    res.locals.results = results;
+    res.locals.hobby = hobbyUnique;
+    res.locals.mood = moodUnique;
+    res.locals.persona = personaUnique;
+    res.render('series-game/overview');
+})
+
 module.exports = router;

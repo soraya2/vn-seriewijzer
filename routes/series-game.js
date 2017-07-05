c11a928d84fff7f63e57ea8f3c21e88e53e2c697
Shyanta Vleugel <shyantavleugel@gmail.com>
2017-07-05 13:01:21 +0200

    Push to active user

--- routes/series-game.js
+++ routes/series-game.js
@@ -185,7 +185,7 @@ router.get('/overview', function (req, res) {
 
     // Clear the matches array in the database at the logged in user
     User.findOneAndUpdate( {
-        'user.facebook.email' : 'shyantavleugel@gmail.com'
+        'user.facebook.email' : req.session.email
     }, {
         '$set' : {
             'user.profile.matches' : []
@@ -197,7 +197,7 @@ router.get('/overview', function (req, res) {
     });
     // Push the best results in the matches array in the database at the logged in user
     User.findOneAndUpdate( {
-        'user.facebook.email' : 'shyantavleugel@gmail.com'
+        'user.facebook.email' : req.session.email
     }, {
         '$push' : {
             'user.profile.matches' : bestResults
